49e872776a240cfd152ececcd85a2f4999044a84
LeonardMccaig <LeonardMccaig@Outlook.com>
2025-04-06 01:03:42 +0100

    Update Serverhoster.py
    Improved Connection method

--- Serverhoster.py
+++ Serverhoster.py
@@ -1,37 +1,25 @@
-import threading
 import socket
-
-import time 
-
-encrypt_string = "AKSDLLMIIIN"
-
-password = encrypt_string[0] + encrypt_string[3] + encrypt_string[6] + encrypt_string[7] + encrypt_string[10] 
-host = '192.168.1.123' # local host
-
-
-while True:
-    x = str(input("Password"))
-    x = x.upper()
-    if x != "ADMIN":
-        input("incorrect Password")
-    else:
-        print("\n  Access Granted")
-        break
-
-port = int(input("enter a port "))
-servername = input("What do you want your room to be called")
+import threading
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host,port))
+server.bind(("localhost", 9999))
 server.listen()
-print("ver 1.7")
+
 clients = []
 nicknames = []
 
 def broadcast(message):
     for client in clients:
         client.send(message)
+
+def sendclients2client(client):
+    client.send("Clients in the room:\n".encode('ascii'))
     
+    for nickname in nicknames:
+        client.send(f" [{nickname}]\n".encode('ascii'))
+
+
+        
 
 def handle(client):
     while True:
@@ -44,31 +32,26 @@ def handle(client):
             client.close()
             nickname = nicknames[index]
             nicknames.remove(nickname)
-            broadcast(f"{nickname} Has left  {servername}".encode('ascii'))
-            
-
+            broadcast(f"\n {nickname} has left the room ".encode("ascii"))
+            break
 
 def receive():
     while True:
-        client, address = server.accept()
+        client, addr = server.accept()
+        print("Client connected with IP", addr)
 
-        
-
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
+        client.send('NICK'.encode("ascii"))
+        nickname = client.recv(1024).decode("ascii")
         nicknames.append(nickname)
-        clients.append(client)        
+        clients.append(client)
 
-
-        print(f"the Nick of the client is {nickname} they jsut connected to {servername}")
-
-        broadcast(f"{nickname} Just joined {servername} \n All Connected users {nicknames}".encode('ascii'))
-        client.send("".encode('ascii'))
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname} just joined the Room!\n ".encode("ascii"))
+        
+        sendclients2client(client)
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
-
-
-print("server is listening... ")
+print("Listening on port 9999...")
 receive()
